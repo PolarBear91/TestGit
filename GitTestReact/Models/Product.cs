4e50c2ec4ad145d4d20e2f44da24ec5db3859186
PolarBear91 <stereo.polar.bear@gmail.com>
2021-03-08 15:18:55 +0300

    create class

---
+++ GitTestReact/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitTestReact.Models
+{
+
+    // public string UrlImage { get; set; }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public int IdCategory { get; set; }
+    }
+}
